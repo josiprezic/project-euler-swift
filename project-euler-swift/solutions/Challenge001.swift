132e8db5e20b6c6b5fb854a7cc8ec198a3f9316c
Josip Rezić <josip.rezic@hotmail.com>
2022-01-01 22:17:04 +0100

    Update Challenge001, use isMultiple(of:)

--- project-euler-swift/solutions/Challenge001.swift
+++ project-euler-swift/solutions/Challenge001.swift
@@ -11,7 +11,7 @@ struct Challenge001: Challenge {
     func solve() -> Int {
         var sum = 0
         Array(1..<1000).forEach {
-            guard $0 % 3 == 0 || $0 % 5 == 0 else { return }
+            guard $0.isMultiple(of: 3) || $0.isMultiple(of: 5) else { return }
             sum += $0
         }
         return sum
