1fdea85f3cbe8ab9bf1ab87161cd3a7c36f58eb1
Josip Rezić <josip.rezic@hotmail.com>
2022-01-02 11:15:17 +0100

    Configure time measurement

--- project-euler-swift/main.swift
+++ project-euler-swift/main.swift
@@ -7,8 +7,9 @@
 
 import Foundation
 
-var challenges = [
-    Challenge001.self
+var challenges: [Challenge] = [
+    Challenge001(),
+    Challenge002()
 ]
 
 print("Challenge number (1-\(challenges.count)): ")
@@ -17,9 +18,17 @@ if let input = readLine(),
    let challengeId = Int(input),
    challengeId > 0,
    challengeId <= challenges.count {
-    let challenge = challenges[challengeId - 1].init()
-    let solution = challenge.solve()
-    print("Solution: \(solution)")
+    let challenge = challenges[challengeId - 1]
+    printResults(for: challenge)
 } else {
     print("Invalid parameter")
+}
+
+func printResults(for challenge: Challenge) {
+    let startTime = CFAbsoluteTimeGetCurrent()
+    let solution = challenge.solve()
+    print("Solution: \(solution)")
+    let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+    let timeElapsedRounded = round(timeElapsed * 1_000) / 1_000
+    print("Time elapsed: \(timeElapsedRounded) seconds")
 }
