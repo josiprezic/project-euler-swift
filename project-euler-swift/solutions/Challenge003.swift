cc79a499dd593f62a4647fab815885481e0c2677
Josip Rezić <josip.rezic@hotmail.com>
2022-01-03 07:57:33 +0100

    Add Challenge003 solution

---
+++ project-euler-swift/solutions/Challenge003.swift
@@ -0,0 +1,26 @@
+//
+//  Challenge003.swift
+//  project-euler-swift
+//
+//  Created by Josip Rezic on 03/01/2022.
+//
+
+import Foundation
+
+struct Challenge003: Challenge {
+    let input = 600_851_475_143
+    
+    func solve() -> Int {
+        var inputCopy = input
+        var quotient = 2
+        
+        while quotient <= inputCopy {
+            if inputCopy.isMultiple(of: quotient) {
+                inputCopy /= quotient
+            } else {
+                quotient += 1
+            }
+        }
+        return quotient
+    }
+}
