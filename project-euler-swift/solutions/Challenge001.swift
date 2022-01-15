0320006de1263606313e8865fa001521e136d39b
Josip Rezić <josip.rezic@hotmail.com>
2022-01-15 17:47:46 +0100

    Update Challenge001, use filter and reduce

--- project-euler-swift/solutions/Challenge001.swift
+++ project-euler-swift/solutions/Challenge001.swift
@@ -11,11 +11,8 @@ struct Challenge001: Challenge {
     let arrayMax = 1_000
     
     func solve() -> Int {
-        var sum = 0
-        Array(1..<arrayMax).forEach {
-            guard $0.isMultiple(of: 3) || $0.isMultiple(of: 5) else { return }
-            sum += $0
-        }
-        return sum
+        (1..<arrayMax)
+            .filter { $0.isMultiple(of: 3) || $0.isMultiple(of: 5) }
+            .reduce(0, +)
     }
 }
