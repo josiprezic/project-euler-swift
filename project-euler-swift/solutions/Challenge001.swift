bad55629117309ae76208ed1f51dbbfeef6fb73c
Josip Rezić <josip.rezic@hotmail.com>
2022-02-20 11:55:50 +0100

    Update Challenge001, add challenge description

--- project-euler-swift/solutions/Challenge001.swift
+++ project-euler-swift/solutions/Challenge001.swift
@@ -7,12 +7,21 @@
 
 import Foundation
 
+// Challenge description: https://projecteuler.net/problem=1
+
 struct Challenge001: Challenge {
+    
+    // number of candidates
     let arrayMax = 1_000
     
     func solve() -> Int {
+        // creating a range of the candidate numbers
         (1..<arrayMax)
+        
+            // filtering all the multiplies of 3 and 5
             .filter { $0.isMultiple(of: 3) || $0.isMultiple(of: 5) }
+        
+            // reducing them to get the result sum
             .reduce(0, +)
     }
 }
