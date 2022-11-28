257b477c6c751458ff10be6b9656f59e2ab12610
Josip Rezić <josip.rezic@hotmail.com>
2022-11-28 21:39:08 +0100

    Update Challenge004 description

--- project-euler-swift/solutions/Challenge004.swift
+++ project-euler-swift/solutions/Challenge004.swift
@@ -19,10 +19,15 @@ struct Challenge004: Challenge {
         // a palindromic number reads the same both ways
         var maxPolindrome = 0
         
+        // iterating over first factor
         for factorOne in factorMin...factorMax {
+            // iterating over second factor
             for factorTwo in factorOne...factorMax {
+                // calculating product candidate
                 let product = factorOne * factorTwo
+                // checking if the product is the largest palindrome we've found
                 guard product > maxPolindrome, product.isPalindrome else { continue }
+                // if so, store it's value to the maxPalindrome
                 maxPolindrome = product
             }
         }
