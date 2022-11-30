541760382540369b48db9e1ae4425364611119f3
Josip Rezić <josip.rezic@hotmail.com>
2022-11-30 23:19:45 +0100

    Add Solution003 description

--- project-euler-swift/solutions/Challenge003.swift
+++ project-euler-swift/solutions/Challenge003.swift
@@ -16,15 +16,21 @@ struct Challenge003: Challenge {
     
     func solve() -> Int {
         var inputCopy = input
+        
+        // initializing quotient variable and assigning the lowest divider value to it
         var quotient = 2
         
         while quotient <= inputCopy {
+            // dividing input with quotient value if possible
             if inputCopy.isMultiple(of: quotient) {
                 inputCopy /= quotient
             } else {
+                // and if not, incrementing the quotient and trying again
                 quotient += 1
             }
         }
+        // at the end we have a number non-divisable by any number
+        // apart from values 1 and it's own value, meaning that it's a prime number
         return quotient
     }
 }
