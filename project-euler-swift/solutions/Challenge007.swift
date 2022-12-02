e82b1ee87a3dc08e639ee49bdcc13c53dea4a59c
Josip Rezić <josip.rezic@hotmail.com>
2022-12-02 21:52:14 +0100

    Update Challenge007 description

--- project-euler-swift/solutions/Challenge007.swift
+++ project-euler-swift/solutions/Challenge007.swift
@@ -15,19 +15,29 @@ struct Challenge007: Challenge {
     let resultPrimeId = 10_001
     
     func solve() -> Int {
+        // initializing the list of prime numbers
         var primesFound = [2]
+        
+        // initializing prime candidate
         var currentPrimeCandidate = 3
         
+        // repeat until finding the prime on the required possition
         while primesFound.count < resultPrimeId {
+            // add number to the primes list if is prime
             if isPrime(currentPrimeCandidate, primesFound: primesFound) {
                 primesFound.append(currentPrimeCandidate)
             }
+            
+            // adding two to get odd numbers since all even numbers
+            // greater than two are not prime numbers
             currentPrimeCandidate += 2
         }
         
         return primesFound[resultPrimeId - 1]
     }
     
+    // returns true if the provided number is prime. Additional parameter divisorCandidates is
+    // used in optimization purposes
     private func isPrime(_ primeCandidate: Int, primesFound divisorCandidates: [Int]) -> Bool {
         for currentDivisor in divisorCandidates {
             guard primeCandidate >= currentDivisor * currentDivisor else { return true }
