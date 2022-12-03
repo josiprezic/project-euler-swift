a6984ead087870ec2341f86f0126e06295dd90e8
Josip Rezić <josip.rezic@hotmail.com>
2022-12-03 19:06:37 +0100

    Update Challenge005 description

--- project-euler-swift/solutions/Challenge005.swift
+++ project-euler-swift/solutions/Challenge005.swift
@@ -16,17 +16,29 @@ struct Challenge005: Challenge {
     let divisorMax = 20
     
     func solve() -> Int {
+        // creating array of divisors
         let divisors = CArray(divisorMin..<divisorMax)
+        
+        // calculating the result using the least common multiple algorithm
         return divisors.reduce(divisorMax, leastCommonMultiple)
     }
     
     private func leastCommonMultiple(of number1: Int, _ number2: Int) -> Int {
+        // find larger number
         let increaseFactor = max(number1, number2)
+        // find lower number
         let checkFactor = min(number1, number2)
+        
+        // initialize result with the larger number value
         var result = increaseFactor
+        
+        // increase result by increase factor (larger number)
+        // until getting the required result
         while !result.isMultiple(of: checkFactor) {
             result += increaseFactor
         }
+        
+        // return least common multiple
         return result
     }
 }
